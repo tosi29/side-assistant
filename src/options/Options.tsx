1297207f5aa33d9a3a9c44a84de314f32c052e2d
tosi29 <tosi16@gmail.com>
2025-02-11 21:25:15 +0900

    API KEYの設定をOption画面に移行

--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -1,7 +1,43 @@
+import { getBucket } from '@extend-chrome/storage';
+import { useEffect, useState } from 'react';
+
+interface ConfigurationBucket {
+  apiKeyGemini: string | null;
+}
+
+const bucket = getBucket<ConfigurationBucket>('my_bucket', 'sync');
+
 const Options = () => {
+  const [apiKeyGemini, setApiKeyGemini] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const value = await bucket.get();
+      if (value.apiKeyGemini) {
+        setApiKeyGemini(value.apiKeyGemini);
+      }
+    })();
+  }, []);
+
+  const saveApiKeyGemini = (apiKeyGemini: string | null) => {
+    bucket.set({ apiKeyGemini: apiKeyGemini });
+    setApiKeyGemini(apiKeyGemini);
+  };
+
   return (
     <div className="flex h-screen items-center justify-center">
       <h1>Options</h1>
+
+      <br />
+      <span>
+        Gemini API KEY:
+        <input
+          type="password"
+          value={apiKeyGemini ?? ''}
+          onChange={(e) => saveApiKeyGemini(e.target.value)}
+          placeholder="Input Gemini API Key"
+        />
+      </span>
     </div>
   );
 };
--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,44 +1,9 @@
-import { getBucket } from '@extend-chrome/storage';
-import { useEffect, useState } from 'react';
-
-interface ConfigurationBucket {
-  apiKeyGemini: string | null;
-}
-
-const bucket = getBucket<ConfigurationBucket>('my_bucket', 'sync');
-
 const Popup = () => {
   document.body.className = 'w-[30rem] h-[15rem]';
 
-  const [apiKeyGemini, setApiKeyGemini] = useState<string | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      const value = await bucket.get();
-      if (value.apiKeyGemini) {
-        setApiKeyGemini(value.apiKeyGemini);
-      }
-    })();
-  }, []);
-
-  const saveApiKeyGemini = (apiKeyGemini: string | null) => {
-    bucket.set({ apiKeyGemini: apiKeyGemini });
-    setApiKeyGemini(apiKeyGemini);
-  };
-
   return (
     <>
-      <div className="flex justify-center mt-2 text-base">Configurations </div>
-      <br />
-      <span>
-        Gemini API KEY:
-        <input
-          type="password"
-          value={apiKeyGemini ?? ''}
-          onChange={(e) => saveApiKeyGemini(e.target.value)}
-          placeholder="Input Gemini API Key"
-        />
-      </span>
+      <div className="flex justify-center mt-2 text-base">Popup</div>
     </>
   );
 };
