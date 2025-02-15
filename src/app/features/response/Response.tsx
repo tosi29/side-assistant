f8091b47aa3751f609ac3e5dca267eed572b7ddc
tosi29 <tosi16@gmail.com>
2025-02-16 01:19:24 +0900

    Responseのスタイルを改善

--- src/app/features/response/Response.tsx
+++ src/app/features/response/Response.tsx
@@ -11,7 +11,7 @@ type ResponseProps = {
 
 const Response: React.FC<ResponseProps> = ({ markdownText }) => {
   return (
-    <div className={`px-4 py-2 ${styles.markdownBody}`}>
+    <div className={`px-4 py-2 mx-2 my-2 ${styles.markdownBody} bg-green-100 rounded-lg`}>
       <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>
         {markdownText}
       </ReactMarkdown>
